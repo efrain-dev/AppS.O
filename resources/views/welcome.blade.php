59a1f5ef1e482305d3d144759bbf009acf085b63
Efrain de Leon <efraindeleon12@outlook.com>
2020-10-10 00:51:33 -0600

    Mejorando plantilla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,143 +1,206 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Inicio</title>
+
+    <title>O.S.S</title>
+    <!--
+
+    Template 2106 Soft Landing
+
+    http://www.tooplate.com/view/2106-soft-landing
+
+    -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="team" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="icon" type="image/png" href="{{asset('imagenes/so.jpg')}}" />
 
-    <style>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 
-        body{
-
-            background:-webkit-linear-gradient(left top, #45B39D, #FF502A );
-        }
-    </style>
-
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/tooplate-style.css">
 
 </head>
 <body>
 
-<script>
+<!-- PRE LOADER -->
+<section class="preloader">
+    <div class="spinner">
 
-    $(document).ready(function(){
-        $('.navContainer').load('Estilo/nav.html');
+        <span class="spinner-rotate"></span>
 
-    });
+    </div>
+</section>
 
-</script>
 
-<div class=" navContainer"></div>
-<div class="container">
+<!-- MENU -->
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+    <div class="container">
 
-    <div style="text-align: center;">
-        <div class="progress">
-            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 100%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+            </button>
+
+            <!-- lOGO TEXT HERE -->
+            <a href="{{route('welcome')}}" class="navbar-brand">Bienvenido a O.S.S</a>
         </div>
-        <h1>Bienvenido a <span class="badge badge-dark">O.S.S</span></h1>
-        <div class="progress">
-            <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+
+        <!-- MENU LINKS -->
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#home" class="smoothScroll">Home</a></li>
+                <li><a href="#feature" class="smoothScroll">Noticias</a></li>
+                <li><a href="#about" class="smoothScroll">About us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Saludame a- <span>info@outlook.com</span></a></li>
+            </ul>
         </div>
 
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item">
-                    <img src="imagenes/Sin título-1.png" class="d-block w-50" alt="">
+    </div>
+</section>
+
+
+<!-- FEATURE -->
+<section id="home" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-offset-3 col-md-6 col-sm-12">
+                <div class="home-info">
+                    <h3>Bienvenido a O.S.S</h3>
+                    <h1>Aqui encontraras informacion en lo ultimo en tecnologia</h1>
+
                 </div>
-                <div class="carousel-item">
-                    <img src="imagenes/ryzer.png" class="d-block w-50" alt="">
-                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<!-- FEATURE -->
+<section id="feature" data-stellar-background-ratio="0.5">
+    <div class="container">
 
                 <div class="carousel-item active">
+                    <img src="{{asset('imagenes/tarjeta_intel.jpg')}}" style="height: 80vh" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block text-capitalize" style="color: black">
+                        <h5>Nuevas graficas de intel</h5>
+                        <p style="color: black">La arquitectura gráfica Intel Xe será el nuevo intento de la compañía azul para competir directamente contra AMD y NVIDIA en todos los segmentos. Intel ha priorizado esta arquitectura inicialmente hacia el segmento de los gráficos integrados para portátiles, y los primeros productos que la llevarán serán los procesadores Tiger Lake (Iris Xe Graphics) y los chips DG1 dedicados para portátiles.</p>
+                        <a href="https://hardzone.es/noticias/tarjetas-graficas/tarjetas-graficas-intel-xe-lanzamiento" style="color: blue" target="_blank"> Mas informacion</a>
+                        <link >
 
-                    <img src="imagenes/nvidia.png" class="d-block w-50" alt="">
+                    </div>
                 </div>
 
-                <div class="carousel-item">
-                    <img src="imagenes/intel.png" class="d-block w-50" alt="">
+    </div>
+</section>
+
+
+<!-- ABOUT -->
+<section id="about" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row">
+
+            <div class=" col-md-12 col-sm-12">
+                <div class="section-title">
+                    <h1>Somos un equipo profesional de la tecnologia</h1>
                 </div>
-
-
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-        <br>
-        <div class="spinner-border text-primary" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-secondary" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-success" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-danger" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-warning" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-info" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-light" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <div class="spinner-border text-dark" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-        <br>
-        <h1>Video Explicativo Sobre <span class="badge badge-success">Procesadores</span></h1>
 
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/S-NCGgLDz9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="col-md-3 col-sm-4">
+                <div class="team-thumb">
+                    <img src="{{asset('imagenes/foto_efrain.jpg')}}" class="img-responsive" alt="Catherine Soft">
+                    <div class="team-info team-thumb-up">
+                        <h2>Efrain de Leon</h2>
+                        <small>CEO/Fundador</small>
+                        <p>Cuenta con una experiencia en motivacion de su equipo y administrar sus recuersos</p>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-md-3 col-sm-4">
+                <div class="team-thumb">
+                    <div class="team-info team-thumb-down">
+                        <h2>Edwin Barrera</h2>
+                        <small>Programador</small>
+                        <p>Cuenta con una larga experiencia en programacion</p>
+                    </div>
+                    <img src="{{asset('imagenes/foto_edwin.jfif')}}" style="height: 45vh" class="img-responsive" alt="Catherine Soft">
+                </div>
+            </div>
 
+            <div class="col-md-3 col-sm-4">
+                <div class="team-thumb">
+                    <img src="{{asset('imagenes/foto_mynor.jfif')}}" class="img-responsive" alt="Catherine Soft">
+                    <div class="team-info team-thumb-up">
+                        <h2>Mynor Pinto</h2>
+                        <small>Escritor Principal/Diseñador</small>
+                        <p>Encargado en el manejo de las notas y su diseño.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="team-thumb">
+                    <div class="team-info team-thumb-down">
+                        <h2>Cesar Miron</h2>
+                        <small>Experto en Marketing</small>
+                        <p>Nuestro especialista en posicionamiento en la web.</p>
+                    </div>
+                    <img src="{{asset('imagenes/foto_cesar.jpg')}}" class="img-responsive" alt="Catherine Soft">
+                </div>
+            </div>
         </div>
-
-
     </div>
-</div>
+</section>
 
 
-<br>
-<br>
-<div class="container-fluid">
 
 
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 100%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+<!-- FOOTER -->
+<footer id="footer" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row">
+
+            <div class="copyright-text col-md-12 col-sm-12">
+                <div class="col-md-6 col-sm-6">
+                    <p>Copyright &copy; 2020 O.S.S </p>
+                </div>
+
+                <div class="col-md-6 col-sm-6">
+                    <ul class="social-icon">
+                        <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                        <li><a href="#" class="fa fa-twitter"></a></li>
+                        <li><a href="#" class="fa fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
     </div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 100%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 100%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
-</div>
+</footer>
+
+<script>
+    $('.carousel').carousel()
+</script>
+<!-- SCRIPTS -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/custom.js"></script>
 
 </body>
 </html>
